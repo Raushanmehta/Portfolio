a7381996b6c38c949763025eca39f9b1552c03c1
Raushan Mehta <142925155+Raushanmehta@users.noreply.github.com>
2024-11-25 06:20:59 +0000

    add

--- js/main.js
+++ js/main.js
@@ -71,23 +71,19 @@
 
 
     // Modal Video
-    $(document).ready(function () {
-        var $videoSrc;
-    
-        $('.btn-play').click(function () {
-            $videoSrc = $(this).data("src");
-            console.log($videoSrc);
-        });
-    
-        $('#videoModal').on('shown.bs.modal', function () {
-            $("#videoFrame").attr('src', $videoSrc + "?autoplay=1&modestbranding=1&showinfo=0");
-        });
-    
-        $('#videoModal').on('hide.bs.modal', function () {
-            $("#videoFrame").attr('src', '');
-        });
+    var $videoSrc;
+    $('.btn-play').click(function () {
+        $videoSrc = $(this).data("src");
     });
-    
+    console.log($videoSrc);
+    $('#videoModal').on('shown.bs.modal', function (e) {
+        $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+    })
+    $('#videoModal').on('hide.bs.modal', function (e) {
+        $("#video").attr('src', $videoSrc);
+    })
+
+
     // Facts counter
     $('[data-toggle="counter-up"]').counterUp({
         delay: 10,
